55dcced397ab56268f2851d5296abbcd7d398e68
gcunsolo <peqpone@gmail.com>
2023-01-15 15:43:20 +0100

    fail on purpose

--- src/components/__tests__/HelloWorld.spec.ts
+++ src/components/__tests__/HelloWorld.spec.ts
@@ -7,5 +7,6 @@ describe('HelloWorld', () => {
   it('renders properly', () => {
     const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' } })
     expect(wrapper.text()).toContain('Hello Vitest')
+    expect(true).toBe(false)
   })
 })
