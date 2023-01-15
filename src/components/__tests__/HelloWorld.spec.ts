bd802595166c156d08702b73773deb805b02ef7b
gcunsolo <peqpone@gmail.com>
2023-01-15 16:03:07 +0100

    fix it

--- src/components/__tests__/HelloWorld.spec.ts
+++ src/components/__tests__/HelloWorld.spec.ts
@@ -7,6 +7,5 @@ describe('HelloWorld', () => {
   it('renders properly', () => {
     const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' } })
     expect(wrapper.text()).toContain('Hello Vitest')
-    expect(true).toBe(false)
   })
 })
