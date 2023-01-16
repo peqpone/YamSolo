632229d8263359e1086c975d352d8f7eafaab448
gcunsolo <peqpone@gmail.com>
2023-01-16 20:46:54 +0100

    setup coverage()

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
@@ -10,5 +10,16 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('src', import.meta.url)),
     },
+  },
+  test: {
+    // deps: { inline: ['@vue', 'vue', 'jsdom'] },
+    coverage: {
+      reporter: ['html', 'text-summary'],
+      include: ['src/**.{js,vue,ts}', 'src/**/**.{js,vue,ts}'],
+      exclude: ['src/**.spec.js', 'src/**/**.spec.js', 'src/__tests__', 'src/**/__tests__'],
+      clean: true,
+      all: true,
+    },
+    root: 'src',
   },
 });
