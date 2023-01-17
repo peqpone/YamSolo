f108495fd4d784dda683583f6ec2af5b0e6bc255
gcunsolo <peqpone@gmail.com>
2023-01-17 19:23:07 +0100

    all the small things

---
+++ src/views/GameView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="about">
+    <h1>Game on</h1>
+  </div>
+</template>
+
+<style>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,33 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue';
+import { reactive } from 'vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+const player = {
+  name: '',
+};
+
+const state = reactive({
+  players: [{ ...player }],
+});
+
+const addPlayer = ():void => {
+  state.players.push({ ...player });
+};
+const startGame = ():void => {
+  router.push({ name: 'game' });
+};
+
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div id="players-customization">
+    <h2>Welcome</h2>
+    <div v-for="(player, key) in state.players" :key="key">
+      <input aria-label="Name for player {{key + 1}}" type="text" v-model="player.name" />
+    </div>
+    <a @click="addPlayer" @keydown="addPlayer">add</a>
+    <a @click="startGame" @keydown="startGame">start</a>
+  </div>
 </template>
