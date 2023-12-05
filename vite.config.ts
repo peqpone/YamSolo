fb1472215a381c5b415b97e59066b48fce392067
gcunsolo <peqpone@gmail.com>
2023-12-05 22:20:10 +0100

    update tests and packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vitest/config';
+import {configDefaults, defineConfig} from 'vitest/config';
 import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
@@ -12,4 +12,32 @@ export default defineConfig({
     },
   },
   assetsInclude: ['src/graphics/dice/**/*.svg'],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    exclude: [...configDefaults.exclude, 'e2e/*'],
+    root: fileURLToPath(new URL('./', import.meta.url)),
+    coverage: {
+      clean: true,
+      reporter: ['html', 'text-summary'],
+      include: ['src/**.{js,vue,ts}', 'src/**/**.{js,vue,ts}'],
+      exclude: [
+        'src/**.spec.js',
+        'src/**/**.spec.js',
+        'src/__tests__',
+        'src/coverage',
+        'src/**/__tests__',
+        'src/router/index.ts',
+        'src/main.ts',
+        'src/App.vue',
+      ],
+      thresholds: {
+        autoUpdate: true,
+        statements: 82.36,
+        branches: 96.35,
+        functions: 84.21,
+        lines: 82.36,
+      },
+    },
+  },
 });
